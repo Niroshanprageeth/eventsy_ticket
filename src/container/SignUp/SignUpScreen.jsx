dd532bf0e8921498efda1a18c5f0b878a07b4b1b
Niroshanprageeth <niroshanprageeth123@gmail.com>
2023-10-27 21:48:24 +0530

    code updated

--- src/container/SignUp/SignUpScreen.jsx
+++ src/container/SignUp/SignUpScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './SignUpScreen.css'; // Import the CSS file
+import './SignUpScreen.css';
 
 const SignUpScreen = ({ closePopup }) => {
   const [name, setName] = useState('');
@@ -13,37 +13,47 @@ const SignUpScreen = ({ closePopup }) => {
   };
 
   return (
-    <div className="container" id='signup'>
+    <div className="container" id="signup">
       <div className="form-container">
         <div className="close-button" onClick={closePopup}>
           <span className="close-icon">&times;</span>
         </div>
         <h2 className="centered-h2">Sign Up</h2>
         <form className="form" onSubmit={handleSignUp}>
-          <input
-            className="input"
-            type="text"
-            placeholder="Full Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            className="input"
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            className="input"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button className="submit-button" type="submit">Sign Up</button>
+          <div className="input-container">
+            <input
+              className="input"
+              type="text"
+              placeholder="Full Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="input-container">
+            <input
+              className="input"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="input-container">
+            <input
+              className="input"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button className="submit-button" type="submit">
+            Sign Up
+          </button>
         </form>
-        <p>Already have an account? <a href="#login">Log In</a></p>
+        <p>
+          Already have an account? <a href="#login">Log In</a>
+        </p>
       </div>
     </div>
   );
